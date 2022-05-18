655abf4a3777affa456ffe9aa21ceb57c3fb0138
zestairlove <zestairlove@gmail.com>
2022-05-18 18:01:30 +0900

    init

---
+++ src/module.ts
@@ -0,0 +1,5 @@
+import { PanelPlugin } from '@grafana/data';
+import { SimpleOptions } from './types';
+import { SimplePanel } from './SimplePanel';
+
+export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).useFieldConfig();
